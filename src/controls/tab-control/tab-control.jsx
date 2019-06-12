c55625a27f10cfb6b6a6c28207445bec08664707
DingpingZhang <DingpingZhang@outlook.com>
2019-06-12 14:57:21 +0800

    Commit a bug.

--- src/controls/tab-control/tab-control.jsx
+++ src/controls/tab-control/tab-control.jsx
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
 import { TabControlWrapper, TabPanel, TabContent } from './styles';
 
+const viewCache = {
+  tabContents: []
+};
+
 export function TabControl(props) {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   const { children, tabPanelBackground, dataTemplate } = props;
   const dateTemplateProvider = dataTemplate || (data => data);
-  const selectedItem = children[selectedIndex];
+
+  if (!viewCache.tabContents[selectedIndex]) {
+    viewCache.tabContents[selectedIndex] =
+      children[selectedIndex].props.children;
+  }
+
+  const tabContent = viewCache.tabContents[selectedIndex];
+  console.log(tabContent);
+  console.log(`selectedIndex = ${selectedIndex}`);
 
   return (
     <TabControlWrapper>
       <TabPanel background={tabPanelBackground}>
-        {children.map((item, index) => {
-          return (
-            <div key={index} onClick={() => setSelectedIndex(index)}>
-              {dateTemplateProvider(item.props.header, selectedIndex === index)}
-            </div>
-          );
-        })}
+        {children.map((item, index) => (
+          <div key={index} onClick={() => setSelectedIndex(index)}>
+            {dateTemplateProvider(item.props.header, selectedIndex === index)}
+          </div>
+        ))}
       </TabPanel>
-      <TabContent>{selectedItem.props.children}</TabContent>
+      <TabContent>{tabContent}</TabContent>
     </TabControlWrapper>
   );
 }
