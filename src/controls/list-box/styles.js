462d46af00636dcc8508ececf96070d727389664
DingpingZhang <DingpingZhang@outlook.com>
2019-06-14 18:55:21 +0800

    Add the scroll bar style to the ListBox control.

--- src/controls/list-box/styles.js
+++ src/controls/list-box/styles.js
@@ -3,7 +3,30 @@ import styled from 'styled-components';
 export const ListBoxWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  overflow: auto;
-  width: 100%;
-  height: 100%;
+
+  overflow-x: hidden;
+  overflow-y: overlay;
+
+  /* ScrollBar for ListBox */
+  &::-webkit-scrollbar {
+    width: 10px;
+    margin: 0 4px;
+
+    &-track {
+      background: transparent;
+    }
+
+    &-thumb {
+      border-radius: 5px;
+      background: #33333333;
+      border: 3px solid transparent;
+      background-clip: padding-box;
+
+      &:hover,
+      &:active {
+        border-width: 1px;
+        background-color: #33333333;
+      }
+    }
+  }
 `;
